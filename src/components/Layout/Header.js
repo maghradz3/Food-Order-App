530551017de0146ea875103db692437f56412d75
maghradz3 <120563893+maghradz3@users.noreply.github.com>
2023-04-01 23:13:46 +0400

    Making Header and Cart Button

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import mealsImg from "../../Assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Georgian Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="table of food" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
