ca72bdc6371239aa054890920e16990c159b335f
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-05-11 16:47:36 +0400

    change header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
-        <h1>Georgian Meals</h1>
+        <h1>Order Meals</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
